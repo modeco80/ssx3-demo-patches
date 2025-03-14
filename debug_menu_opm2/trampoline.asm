95474c18a74f91aa7e38812bba9be92006cf60c3
modeco80 <lily.modeco80@protonmail.ch>
2025-03-13 21:26:24 -0400

    Sizecode opm2 debug menu patch down to 12*4 bytes

--- debug_menu_opm2/trampoline.asm
+++ debug_menu_opm2/trampoline.asm
@@ -4,20 +4,21 @@
 .ps2
 
 ; TRAMPOLINE FUNCTION
-; This is 15*4 bytes long, although in the space I've found 
+; This is 12*4 bytes long, although in the space I've found 
 ; there's about 25*4 bytes extra space in this cave (past the ones I'm using here)
 .create "trampoline.bin",0x003796d0
 .org 0x003796d0
 trampoline:
-	lui t0,0x0040     		; load address of SSXApp global pointer into t0
-	ori t0,t0,0x009c
-	move t1, a0			; save old a0 into t1 (we will need it later)
-	lw t3, 0(t0) 			; load value of SSXApp pointer into t3
+	lui t0,0x0040     		; load the address of the 
+					; SSXApp global pointer into t0
+	ori t0,t0,0x009c		; ...
+	
+	move t2, zero			; preload t2
+	ori t2, zero, 0x0008		; with 0x8 (value of debug button ID)
 
-	move t2, zero			; check if the debug button was touched here
-	ori t2, zero, 0x0008
-	beq a2, t2, .is_debug_menu_id
-	nop				
+	beq a2, t2, .is_debug_menu_id	; check if the debug button was touched here
+	lw t3, 0(t0) 			; load value of SSXApp pointer into t3
+				
 .not_debug_menu_id:			; t'was not..
 	jr t1				; "return" back via switch table entry, 
 					; and detour back to the normal code path
@@ -26,9 +27,8 @@ trampoline:
 .is_debug_menu_id:			; okay, this is it...
 	lw a0, 0x5c(t3)			; load cGame ptr from SSXApp
 	lw a1, 0			; first controller? not sure what this param is tbh
-	mtc1 zero, f12			; zero unknown float thats required?
 	j 0x001acef0			; jump to debug menu function (it will jr ra for us)
-	nop				; ...
+	mtc1 zero, f12			; zero unknown float thats required?
 .close
 
 ; TRAMPOLINE ENTRY
@@ -36,5 +36,7 @@ trampoline:
 .org 0x001874e8
 trampoline_entry:
 	jal trampoline			; jump to trampoline
-	nop				; ...
+	move t1, a0			; save old a0 into t1
+					; since we'll need it if
+					; the trampoline wants to jump back
 .close
